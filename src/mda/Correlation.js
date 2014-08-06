5ec9a94b0ffa777fd9aee9ed02a5ce2ecc529067
niksoper <nsoper@scottlogic.com>
2014-08-06 12:26:53 +0100

    Comments for correlation calulations.

--- src/mda/Correlation.js
+++ src/mda/Correlation.js
@@ -1,7 +1,19 @@
+/**
+ * Module for calculating correlation coefficients on pairs of values.
+ * @namespace insight.correlation
+ */
 insight.correlation = (function(insight) {
 
     var correlation = {};
 
+    /**
+     * Calculates the pearson correlation coefficient for two arrays of numbers.
+     * The two arrays must be equal in length and must only contain numbers.
+     * @memberof! insight.correlation
+     * @param {Array<Number>} x The x values
+     * @param {Array<Number>} y The y values
+     * @returns {Number} The pearson correlation coefficient for two arrays of numbers
+     */
     correlation.fromValues = function(x, y) {
 
         if (!insight.Utils.isArray(x) || !insight.Utils.isArray(y) ||
@@ -25,6 +37,15 @@ insight.correlation = (function(insight) {
 
     };
 
+    /**
+     * Calculates the pearson correlation coefficients for all given property pairs in the given dataset.
+     * @memberof! insight.correlation
+     * @param {insight.DatSet|Array<Object>} dataset The insight.DataSet or
+     * array to calclulate correlation coefficients for.
+     * @param {Array<String[]>} correlationPairs An array of pairs of property names to calculate correlation coefficients
+     * for. Each element of the array must be an array of two strings.
+     * @returns {Object} An object containing properties for all correlation pairs e.g. { X_Cor_Y: 0.75 }
+     */
     correlation.fromDataSet = function(dataset, correlationPairs) {
 
         var dataArray = getArray(dataset),
@@ -49,12 +70,20 @@ insight.correlation = (function(insight) {
 
     };
 
+    /*
+     * Returns the property name to use for a correlation between two properties, x and y.
+     */
     function correlationPropertyName(x, y) {
 
         return x + '_Cor_' + y;
 
     }
 
+    /*
+     * Adds an array to the container for each element in propertyNames.
+     * An array will not be added to the container object if it has been added before.
+     * Each array will contain the 'propertyName' property from the object in dataArray.
+     */
     function addPropertyArrays(dataArray, container, propertyNames) {
 
         propertyNames.forEach(function(propertyName) {
@@ -71,6 +100,10 @@ insight.correlation = (function(insight) {
 
     }
 
+    /*
+     * Returns an array based on the given object.
+     * If the object is an insight.DataSet then its data is returned, otherwise the array id returned directly.
+     */
     function getArray(obj) {
 
         if (obj instanceof insight.DataSet) {
@@ -85,6 +118,9 @@ insight.correlation = (function(insight) {
 
     }
 
+    /*
+     * Sums the elements of an array;
+     */
     function sum(array) {
 
         return array.reduce(function(previous, current) {
@@ -99,12 +135,19 @@ insight.correlation = (function(insight) {
 
     }
 
+    /*
+     * Calculates the mean of all elements in an array
+     */
     function mean(array) {
 
         return sum(array) / array.length;
 
     }
 
+    /*
+     * Multiplies each element in an array with the corresponding element
+     * in another array and returns a new array with the results.
+     */
     function multiply(array1, array2) {
 
         var multiplied = [];
@@ -119,6 +162,9 @@ insight.correlation = (function(insight) {
 
     }
 
+    /*
+     * Subtracts a constant from each element in an array and returns a new array with the results.
+     */
     function subtract(array, constant) {
 
         return array.map(function(d) {
