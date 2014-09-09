5a8a8b9153e54abcd03c3c2a02122b7ff6595282
Thomas Kelly <tkelly@scottlogic.co.uk>
2014-09-09 16:21:14 +0100

    Make scatter series opaque
    Defaults scatter series to complete opacity.

--- src/charts/Series/ScatterSeries.js
+++ src/charts/Series/ScatterSeries.js
@@ -34,6 +34,8 @@
             return data;
         };
 
+        // Override default opacity to be opaque
+        self.pointOpacity(1.0);
     };
 
     insight.ScatterSeries.prototype = Object.create(insight.PointSeries.prototype);
