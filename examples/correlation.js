0531b509d883285db7858d8730000c9b079344bf
Matthew Dunsdon <mdunsdon@scottlogic.com>
2014-09-10 16:28:02 +0100

    Remove manual margins from examples

--- examples/correlation.js
+++ examples/correlation.js
@@ -24,14 +24,7 @@ $(document)
 
             var scatterChart = new insight.Chart('Chart 3', '#bubbleChart')
                 .width(500)
-                .height(400)
-                .margin(
-                {
-                    top: 10,
-                    left: 150,
-                    right: 40,
-                    bottom: 80
-                });
+                .height(400);
 
             var xAxis = new insight.Axis('Average Number of Ratings', insight.Scales.Linear)
                 .tickSize(2)
