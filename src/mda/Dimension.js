492bc044600b01cc283ba90b6827c053226e981c
Thomas Kelly <tkelly@scottlogic.co.uk>
2014-09-23 10:37:10 +0100

    Remove Dimension @todos
    These are now in Jira as ICS-246 and ICS-247

--- src/mda/Dimension.js
+++ src/mda/Dimension.js
@@ -3,8 +3,6 @@
      * A Dimension organizes a dataset along a particular property, or variation of a property.
      * Defining a dimension with a function of:<pre><code>function(d){ return d.Surname; }</code></pre> will slice a dataset by the distinct values of the Surname property.
      * @constructor
-     * @todo reimplement how Dimensions are created.  Too much is inside ChartGroup at the moment, and ChartGroup is becoming redundant and too mixed
-     * @todo display function should be provided by a setter.
      * @param {String} name - The short name used to identify this dimension, and any linked dimensions sharing the same name
      * @param {Object} crossfilterData - The crossfilter object to create the Dimension on.
      * @param {Function} sliceFunction - The function used to categorize points within the dimension.
