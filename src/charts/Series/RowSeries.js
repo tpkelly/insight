a5163c0f0259a14271c56302af484b807c2944f1
Thomas Kelly <tkelly@scottlogic.co.uk>
2014-09-23 15:09:51 +0100

    Remove colour from RowSeries
    We don't use the colour anymore, so shouldn't document it.

--- src/charts/Series/RowSeries.js
+++ src/charts/Series/RowSeries.js
@@ -8,7 +8,6 @@
      * @param {insight.DataSet} data - The DataSet containing this series' data
      * @param {insight.Scales.Scale} x - the x axis
      * @param {insight.Scales.Scale} y - the y axis
-     * @param {Object} color - a string or function that defines the color to be used for the items in this series
      */
     insight.RowSeries = function RowSeries(name, data, x, y) {
 
