32b3a28eb2a426c7f8dd084a260810128ac4ec1d
niksoper <nsoper@scottlogic.com>
2014-09-30 13:30:53 +0100

    Added DataProvider as base container for all InsightJS data

---
+++ src/mda/DataProvider.js
@@ -0,0 +1,114 @@
+(function(insight) {
+    /**
+     * DataProvider is the base container for data to be used within InsightJS. It provides a consistent approach to
+     * sorting and filtering object arrays.
+     * @constructor
+     * @param {Object[]} collection - The data to be processed and represented by this DataProvider.
+     */
+    insight.DataProvider = function DataProvider(collection) {
+
+        // Private variables
+        var self = this,
+            orderFunction = null,
+            filterFunction = null;
+
+        // Public functions -------------------------------------------------------------------------------------------
+
+        /**
+         * Returns an object array used as the starting point before sorting and filtering is applied.
+         * @virtual
+         * @memberof! insight.DataProvider
+         * @instance
+         * @returns {Object[]}
+         * */
+        self.rawData = function() {
+            return collection.slice(0);
+        };
+
+        /**
+         * @memberof! insight.DataProvider
+         * @instance
+         * @param {Function} [orderFunc] If provided then the data will be ordered using this function.
+         * @param {Integer} [top] If provided then the number of objects returned will be limited to the top N.
+         * @returns {Object[]}
+         */
+        self.extractData = function(orderFunc, top) {
+
+            if (top <= 0) {
+                return [];
+            }
+
+            var dataArray = self.rawData();
+
+            orderFunc = orderFunc || self.orderingFunction();
+
+            if (self.filterFunction()) {
+                dataArray = dataArray.filter(self.filterFunction());
+            }
+
+            if (orderFunc) {
+                dataArray = dataArray.sort(orderFunc);
+            }
+
+            if (top) {
+                dataArray = dataArray.slice(0, top);
+            }
+
+            return dataArray;
+        };
+
+        /**
+         * Gets the function used to order the data source values
+         * @memberof! insight.DataProvider
+         * @instance
+         * @returns {Function} - The current ordering function
+         *
+         * @also
+         *
+         * Sets the function used to order the data source values
+         * @memberof! insight.DataProvider
+         * @instance
+         * @param {Function} orderFunc The ordering function
+         * @returns {this}
+         */
+        self.orderingFunction = function(orderFunc) {
+
+            if (!arguments.length) {
+                return orderFunction;
+            }
+
+            orderFunction = orderFunc;
+
+            return self;
+        };
+
+        /**
+         * The function to use to filter an object from the data source.
+         * The function should return a boolean where false means the object is not included in the data source.
+         * @memberof! insight.DataProvider
+         * @instance
+         * @returns {Function} - The function to use to filter an object from the data source.
+         *
+         * @also
+         *
+         * Sets the function to use to filter an object from the data source.
+         * The function should return a boolean where false means the object is not included in the data source.
+         * @memberof! insight.DataProvider
+         * @instance
+         * @param {Function} filterFunc The new function to use to filter an object from the data source.
+         * @returns {this}
+         */
+        self.filterFunction = function(filterFunc) {
+
+            if (!arguments.length) {
+                return filterFunction;
+            }
+
+            filterFunction = filterFunc;
+
+            return self;
+        };
+
+    };
+
+})(insight);
---
+++ tests/mda/DataProvider.spec.js
@@ -0,0 +1,290 @@
+describe('DataProvider', function() {
+
+    var data = [
+        { id: 2, key : 'b', value: 'bravo' },
+        { id: 1, key : 'a', value: 'alan' },
+        { id: 3, key : 'c', value: 'charlie' },
+        { id: 5, key : 'e', value: 'echo' },
+        { id: 4, key : 'd', value: 'delta' }
+    ];
+
+    var evenIdFilterFunc = function(d) {
+        return d.id % 2 === 0;
+    };
+
+    var oddIdFilterFunc = function(d) {
+        return d.id % 2 !== 0;
+    };
+
+    var descendingIdOrderFunc = function(a, b) {
+        return b.id - a.id;
+    };
+
+    var ascendingIdOrderFunc = function(a, b) {
+        return a.id - b.id;
+    };
+
+    describe('filterFunction', function() {
+
+        var dataProvider;
+
+        beforeEach(function() {
+            dataProvider = new insight.DataProvider(data);
+        });
+
+        it('is null by default', function() {
+
+            // When:
+            var filterBy = dataProvider.filterFunction();
+
+            // Then:
+            expect(filterBy).toBeNull();
+
+        });
+
+        it('can be overridden', function() {
+
+            // When:
+            dataProvider.filterFunction(evenIdFilterFunc);
+
+            // Then:
+            expect(dataProvider.filterFunction()).toBe(evenIdFilterFunc);
+
+        });
+
+    });
+
+    describe('orderingFunction', function() {
+
+        var dataProvider;
+
+        beforeEach(function() {
+            dataProvider = new insight.DataProvider(data);
+        });
+
+        it('is null by default', function() {
+
+            // When:
+            var orderBy = dataProvider.orderingFunction();
+
+            // Then:
+            expect(orderBy).toBeNull();
+
+        });
+
+        it('can be overridden', function() {
+
+            // When:
+            dataProvider.orderingFunction(descendingIdOrderFunc);
+
+            // Then:
+            expect(dataProvider.orderingFunction()).toBe(descendingIdOrderFunc);
+
+        });
+
+    });
+
+    describe('extractData', function() {
+
+        var dataProvider;
+
+        beforeEach(function(){
+            dataProvider = new insight.DataProvider(data);
+            spyOn(dataProvider, 'rawData').andCallThrough();
+        });
+
+        it('by default gets data using rawData', function() {
+
+            // When:
+            var result = dataProvider.extractData();
+
+            // Then:
+            expect(result).toEqual(data);
+            expect(dataProvider.rawData).toHaveBeenCalled();
+        });
+
+        describe('orders data', function() {
+
+            it('when ordering function supplied directly', function() {
+
+                // When:
+                var result = dataProvider.extractData(descendingIdOrderFunc);
+
+                // Then:
+                var expectedResult = [
+                    { id: 5, key : 'e', value: 'echo' },
+                    { id: 4, key : 'd', value: 'delta' },
+                    { id: 3, key : 'c', value: 'charlie' },
+                    { id: 2, key : 'b', value: 'bravo' },
+                    { id: 1, key : 'a', value: 'alan' }
+                ];
+
+                expect(result).toEqual(expectedResult);
+                expect(dataProvider.rawData).toHaveBeenCalled();
+
+            });
+
+            it('using orderingFunction property if no ordering function is provided directly', function() {
+
+                // Given:
+                dataProvider.orderingFunction(descendingIdOrderFunc);
+
+                // When:
+                var result = dataProvider.extractData();
+
+                // Then:
+                var expectedResult = [
+                    { id: 5, key : 'e', value: 'echo' },
+                    { id: 4, key : 'd', value: 'delta' },
+                    { id: 3, key : 'c', value: 'charlie' },
+                    { id: 2, key : 'b', value: 'bravo' },
+                    { id: 1, key : 'a', value: 'alan' }
+                ];
+
+                expect(result).toEqual(expectedResult);
+                expect(dataProvider.rawData).toHaveBeenCalled();
+
+            });
+
+            it('with supplied ordering function, when orderingFunction property has been set', function() {
+
+                // Given:
+                dataProvider.orderingFunction(descendingIdOrderFunc);
+
+                // When:
+                var result = dataProvider.extractData(ascendingIdOrderFunc);
+
+                // Then:
+                var expectedResult = [
+                    { id: 1, key : 'a', value: 'alan' },
+                    { id: 2, key : 'b', value: 'bravo' },
+                    { id: 3, key : 'c', value: 'charlie' },
+                    { id: 4, key : 'd', value: 'delta' },
+                    { id: 5, key : 'e', value: 'echo' }
+                ];
+
+                expect(result).toEqual(expectedResult);
+                expect(dataProvider.rawData).toHaveBeenCalled();
+
+            });
+
+        });
+
+        describe('when supplied top parameter', function() {
+
+            it('limits number of data objects', function() {
+
+                // When:
+                var result = dataProvider.extractData(null, 3);
+
+                // Then:
+                var expectedData = [
+                    { id: 2, key : 'b', value: 'bravo' },
+                    { id: 1, key : 'a', value: 'alan' },
+                    { id: 3, key : 'c', value: 'charlie' }
+                ];
+
+                expect(result).toEqual(expectedData);
+                expect(dataProvider.rawData).toHaveBeenCalled();
+            });
+
+            it('of value 0 gives empty results', function() {
+
+                // When:
+                var result = dataProvider.extractData(null, 0);
+
+                // Then:
+                expect(result).toEqual([]);
+            });
+
+            it('of negative value gives empty results', function() {
+
+                // When:
+                var result = dataProvider.extractData(null, -3);
+
+                // Then:
+                expect(result).toEqual([]);
+            });
+
+            it('applies limit after data has been ordered', function() {
+
+                // When:
+                var result = dataProvider.extractData(ascendingIdOrderFunc, 3);
+
+                // Then:
+                var expectedData = [
+                    { id: 1, key : 'a', value: 'alan' },
+                    { id: 2, key : 'b', value: 'bravo' },
+                    { id: 3, key : 'c', value: 'charlie' }
+                ];
+
+                expect(result).toEqual(expectedData);
+                expect(dataProvider.rawData).toHaveBeenCalled();
+            });
+
+        });
+
+        describe('when filterFunction has been set', function() {
+
+            it('applies filtering to data', function() {
+
+                // Given:
+                dataProvider.filterFunction(evenIdFilterFunc);
+
+                // When:
+                var result = dataProvider.extractData();
+
+                // Then:
+                var expectedData = [
+                    { id: 2, key : 'b', value: 'bravo' },
+                    { id: 4, key : 'd', value: 'delta' }
+                ];
+
+                expect(result).toEqual(expectedData);
+                expect(dataProvider.rawData).toHaveBeenCalled();
+
+            });
+
+            it('filtering takes place before top limit is applied', function() {
+
+                // Given:
+                dataProvider.filterFunction(oddIdFilterFunc);
+
+                // When:
+                var result = dataProvider.extractData(null, 2);
+
+                // Then:
+                var expectedData = [
+                    { id: 1, key : 'a', value: 'alan' },
+                    { id: 3, key : 'c', value: 'charlie' }
+                ];
+
+                expect(result).toEqual(expectedData);
+                expect(dataProvider.rawData).toHaveBeenCalled();
+
+            });
+
+            it('handles when orderingFunction and top parameters are also supplied', function() {
+
+                // Given:
+                dataProvider.filterFunction(oddIdFilterFunc);
+
+                // When:
+                var result = dataProvider.extractData(descendingIdOrderFunc, 2);
+
+                // Then:
+                var expectedData = [
+                    { id: 5, key : 'e', value: 'echo' },
+                    { id: 3, key : 'c', value: 'charlie' }
+                ];
+
+                expect(result).toEqual(expectedData);
+                expect(dataProvider.rawData).toHaveBeenCalled();
+
+            });
+
+        });
+    });
+
+});
+
